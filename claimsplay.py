6c404cc6782fc669f522cf7a8f52af076b74bb14
casshknight25 <100038929+casshknight25@users.noreply.github.com>
2023-02-15 15:03:27 +0000

    Update claimsplay.py

--- claimsplay.py
+++ claimsplay.py
@@ -437,6 +437,20 @@ if data =='Broker Data':
         df_brokers = pd.merge(df,df_b,how='left')
         st.dataframe(df_brokers)
     st.info("We are now able to complete the request - select the rows and columns to create the pivot table showing the average lifecycle and notification times split by Broker name and line of business")
+     def clean_df(data):
+            df = df.drop_duplicates(subset = "Claim reference")
+            df = df.astype({"Claim reference": 'str'})
+            df = df[df['Claim reference'].map(len) > 7]
+            df = df.dropna()
+            df["Broker Account Number"] = df["Broker Account Number"].replace(['PK12488'],['68623'])
+            df["Policy Number"] = df["Policy Number"].replace(['68623'],['PK12488'])
+            df.drop(df.loc[df['Broker Account Number']== 'X'].index, inplace=True)
+            df = df.dropna()
+            return df
+        df_b = pd.read_csv("brokeraccount.csv")
+        df_b.dropna()
+        df["Broker Account Number"] = df["Broker Account Number"].astype(int)
+        df_brokers = pd.merge(df,df_b,how='left')
     row = st.multiselect('Select Rows', ['Broker Account Number', 'Broker Name', 'Line of Business'], key="12")
     column = st.multiselect('Select Columns', [ 'Notification Time', 'Lifecycle'], key="13")
     if row ==['Line of Business'] and column ==['Notification Time', 'Lifecycle']:
@@ -451,6 +465,9 @@ if data =='Broker Data':
         
     st.header("Data Visualisation")
     st.info("A good way to visualise this data with be with use of a box plots. Box plots are useful tools to show the spread of a data set. The 'box' of the plot holds the middle 50% of the data set - the longer the box means the middle values of the data set are spread further apart (i.e. have a wider range of values), if the box is short it means the middle values of the data set are all quite close together. The line that divides the box is the median - this shows the middle value of the date set. The line below the box shows the bottom 25% of values of the data set, the line at the bottom indicates the minimum value of the data set. Likewise the line above the box shows the top 25% of the data set with the line at the top showing the maximum value of the data set.")      
+    df_b = pd.read_csv("brokeraccount.csv")
+    df["Broker Account Number"] = df["Broker Account Number"].astype(int)
+    df_brokers = pd.merge(df,df_b,how='left')
     fig1 = px.box(df_brokers, x='Line of Business', y='Notification Time')
     st.plotly_chart(fig1, use_container_width=True)
     range = st.selectbox("Which Line of Business has the biggest range of notification times?", ('Motor', 'Casualty','PI','Property'))
